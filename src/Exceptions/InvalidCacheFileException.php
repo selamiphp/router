62ecff07680e7e85574b5ed1ffec19251caf5fd4
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2018-12-20 03:01:40 +0300

    New exception introduces [skip ci]

--- src/Exceptions/InvalidCacheFileException.php
+++ src/Exceptions/InvalidCacheFileException.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Selami\Router\Exceptions;
 
-class InvalidRequestMethodException extends \InvalidArgumentException
+class InvalidCacheFileException extends \InvalidArgumentException
 {
 
 }
