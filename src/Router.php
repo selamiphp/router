49b9dccc0f77beb3f519001a804e73205a41ed10
Mehmet Korkmaz <mehmet.korkmaz@reformo.net>
2019-02-26 04:24:49 +0300

    requestedPath rtrimed

--- src/Router.php
+++ src/Router.php
@@ -115,7 +115,7 @@ final class Router
             throw new InvalidRequestMethodException($message);
         }
         $this->method = $method;
-        $this->requestedPath = $requestedPath;
+        $this->requestedPath = rtrim($requestedPath, '/',);
         $this->defaultReturnType = ($defaultReturnType >=1 && $defaultReturnType <=7) ? $defaultReturnType : self::HTML;
     }
 
